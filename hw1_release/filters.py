d5542e42dcf521966917a24b4608dfdf6cf03190
whu-pzhang <pzhang@whu.edu.cn>
2018-08-08 20:25:11 +0800

    hw1 done😀

--- hw1_release/filters.py
+++ hw1_release/filters.py
@@ -19,11 +19,17 @@ def conv_nested(image, kernel):
     Hk, Wk = kernel.shape
     out = np.zeros((Hi, Wi))
 
-    ### YOUR CODE HERE
-    pass
-    ### END YOUR CODE
+    # YOUR CODE HERE
+    for i in range(Hk // 2, Hi - Hk // 2):
+        for j in range(Wk // 2, Wi - Wk // 2):
+            for k in range(Hk):
+                for l in range(Wk):
+                    out[i, j] += kernel[k, l] * \
+                        image[i + Hk // 2 - k, j + Wk // 2 - l]
+    # END YOUR CODE
 
     return out
+
 
 def zero_pad(image, pad_height, pad_width):
     """ Zero-pad an image.
@@ -46,9 +52,10 @@ def zero_pad(image, pad_height, pad_width):
     H, W = image.shape
     out = None
 
-    ### YOUR CODE HERE
-    pass
-    ### END YOUR CODE
+    # YOUR CODE HERE
+    out = np.pad(image, ((pad_height, pad_height), (pad_width,
+                                                    pad_width)), "constant", constant_values=(0))
+    # END YOUR CODE
     return out
 
 
@@ -75,11 +82,22 @@ def conv_fast(image, kernel):
     Hk, Wk = kernel.shape
     out = np.zeros((Hi, Wi))
 
-    ### YOUR CODE HERE
-    pass
-    ### END YOUR CODE
+    # YOUR CODE HERE
+    # 若要和conv_nested的结果做比较的话，这里不应该使用zero_pad函数！
+    # 因为conv_nested中并没有pad操作
+    # image = zero_pad(image, Hk // 2, Wk // 2)  # padding image
+    kernel = np.flip(np.flip(kernel, axis=0), axis=1)  # flip kernel
+    half_Hk = Hk // 2
+    half_Wk = Wk // 2
+    for i in range(half_Hk, Hi - half_Hk):
+        for j in range(half_Wk, Wi - half_Wk):
+            out[i, j] = np.sum(
+                image[i - half_Hk:i + half_Hk + 1, j - half_Wk:j + half_Wk + 1] * kernel)
+
+    # END YOUR CODE
 
     return out
+
 
 def conv_faster(image, kernel):
     """
@@ -94,11 +112,19 @@ def conv_faster(image, kernel):
     Hk, Wk = kernel.shape
     out = np.zeros((Hi, Wi))
 
-    ### YOUR CODE HERE
-    pass
-    ### END YOUR CODE
+    # YOUR CODE HERE
+    kernel = np.flip(np.flip(kernel, axis=0), axis=1)  # flip kernel
+    half_Hk = Hk // 2
+    half_Wk = Wk // 2
+    # 将相乘相加操作变为向量乘法
+    for i in range(half_Hk, Hi - half_Hk):
+        for j in range(half_Wk, Wi - half_Wk):
+            out[i, j] = np.ravel(image[i - half_Hk:i + half_Hk + 1, j -
+                                       half_Wk:j + half_Wk + 1]) @ np.ravel(kernel)
+    # END YOUR CODE
 
     return out
+
 
 def cross_correlation(f, g):
     """ Cross-correlation of f and g
@@ -114,11 +140,14 @@ def cross_correlation(f, g):
     """
 
     out = None
-    ### YOUR CODE HERE
-    pass
-    ### END YOUR CODE
+    # YOUR CODE HERE
+    g = np.flip(np.flip(g, axis=0), axis=1)
+    # change dimension of g to be an odd number
+    out = conv_fast(f, g[:-1, :])
+    # END YOUR CODE
 
     return out
+
 
 def zero_mean_cross_correlation(f, g):
     """ Zero-mean cross-correlation of f and g
@@ -134,11 +163,13 @@ def zero_mean_cross_correlation(f, g):
     """
 
     out = None
-    ### YOUR CODE HERE
-    pass
-    ### END YOUR CODE
+    # YOUR CODE HERE
+    g = g - np.mean(g)
+    out = cross_correlation(f, g)
+    # END YOUR CODE
 
     return out
+
 
 def normalized_cross_correlation(f, g):
     """ Normalized cross-correlation of f and g
@@ -155,8 +186,23 @@ def normalized_cross_correlation(f, g):
     """
 
     out = None
-    ### YOUR CODE HERE
-    pass
-    ### END YOUR CODE
+    # YOUR CODE HERE
+    g = g[:-1, :]  # Make the dimension of g is odd number
+
+    g_normalized = (g - np.mean(g)) / np.std(g)
+
+    Hi, Wi = f.shape
+    Hk, Wk = g.shape
+    half_Hk = Hk // 2
+    half_Wk = Wk // 2
+    out = np.zeros((Hi, Wi))
+
+    for i in range(half_Hk, Hi - half_Hk):
+        for j in range(half_Wk, Wi - half_Wk):
+            f_patch = f[i - half_Hk:i + half_Hk +
+                        1, j - half_Wk:j + half_Wk + 1]
+            out[i, j] = np.sum(
+                (f_patch - np.mean(f_patch)) / np.std(f_patch) * g_normalized)
+    # END YOUR CODE
 
     return out
